360fa5a1b5a5cfaa27b987dd3c0608264542427c
eugenegantz <eugengantz@gmail.com>
2017-09-01 19:46:45 +0300

    void

--- index.js
+++ index.js
@@ -2,6 +2,139 @@
 
 module.exports = {
 
+	biz: {
+
+		tax: {
+
+			taxId: {
+
+				/**
+				 * @param {Array | String} id
+				 * @return {Array}
+				 */
+				_toArrNum(id = []) {
+					if (typeof id === 'string')
+						id = id.trim().split('');
+
+					id.forEach((v, i) => id[i] = +id[i]);
+
+					return id;
+				},
+
+
+				/**
+				 * @param {Array} id
+				 * @return {Boolean}
+				 */
+				_isValidNumSet(id = []) {
+					return id.every(v => isFinite(v));
+				},
+
+
+				/**
+				 * Является ли ИНН валидным
+				 * либо для физ. лица
+				 * либо для юр. лица
+				 * @param {String | Array} id - ИНН
+				 * @return {Boolean}
+				 */
+				isValid(id = []) {
+					if (10 === id.length)
+						return this.isValidForJuridicalPerson(id);
+
+					if (12 === id.length)
+						return this.isValidForNaturalPerson(id);
+
+					return false;
+				},
+
+
+				/**
+				 * Является ли ИНН валидным
+				 * либо для физ. лица
+				 * @param {String | Array} id - ИНН
+				 * @return {Boolean}
+				 */
+				isValidForNaturalPerson(id = []) {
+					id = this._toArrNum(id);
+
+					if (id.length != 12)
+						return false;
+
+					if (!this._isValidNumSet(id))
+						return false;
+
+					let s1 = (
+						(
+							  (7 * id[0])
+							+ (2 * id[1])
+							+ (4 * id[2])
+							+ (10 * id[3])
+							+ (3 * id[4])
+							+ (5 * id[5])
+							+ (9 * id[6])
+							+ (4 * id[7])
+							+ (6 * id[8])
+							+ (8 * id[9])
+						) % 11
+					) % 10;
+
+					let s2 = (
+						(
+							  (3 * id[0])
+							+ (7 * id[1])
+							+ (2 * id[2])
+							+ (4 * id[3])
+							+ (10 * id[4])
+							+ (3 * id[5])
+							+ (5 * id[6])
+							+ (9 * id[7])
+							+ (4 * id[8])
+							+ (6 * id[9])
+							+ (8 * id[10])
+						) % 11
+					) % 10;
+
+					return s1 === id[10] && s2 === id[11];
+				},
+
+
+				/**
+				 * Является ли ИНН валидным
+				 * либо для юр. лица
+				 * @param {String | Array} id - ИНН
+				 * @return {Boolean}
+				 */
+				isValidForJuridicalPerson(id) {
+					id = this._toArrNum(id);
+
+					if (!this._isValidNumSet(id))
+						return false;
+
+					let s1 = (
+						(
+							  (2 * id[0])
+							+ (4 * id[1])
+							+ (10 * id[2])
+							+ (3 * id[3])
+							+ (5 * id[4])
+							+ (9 * id[5])
+							+ (4 * id[6])
+							+ (6 * id[7])
+							+ (8 * id[8])
+						) % 11
+					) % 10;
+
+					return id[9] === s1;
+				}
+
+			}
+
+		}
+
+	},
+
+
 	datetime: {
 
 		/**
